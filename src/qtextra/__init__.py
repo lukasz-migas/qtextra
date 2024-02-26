4df119a04053e61a87bec240b5e72be6dcc0e2ce
lukasz-migas <lukas.migas@yahoo.com>
2024-02-26 11:45:37 +0000

    Update __init__.py

--- src/qtextra/__init__.py
+++ src/qtextra/__init__.py
@@ -5,5 +5,5 @@ __version__ = "0.1.2"
 __author__ = "Lukasz G. Migas"
 __email__ = "lukas.migas@yahoo.com"
 __issue_url__ = "https://github.com/illumion-io/qtextra-issues/issues"
-__project_url__ = "https://ionglow.io"
+__project_url__ = "https://ionglow.io/qtextra"
 logger.disable("qtextra")
