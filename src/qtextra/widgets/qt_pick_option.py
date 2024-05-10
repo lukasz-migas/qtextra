36fa4805dca65a9f7245599bd52a0440d652cd4c
lukasz-migas <lukas.migas@yahoo.com>
2024-05-10 10:02:31 +0100

    Foxes

--- src/qtextra/widgets/qt_pick_option.py
+++ src/qtextra/widgets/qt_pick_option.py
@@ -1,5 +1,7 @@
 """Simple dialog to select between two options."""
 
+from __future__ import annotations
+
 import typing as ty
 from functools import partial
 
@@ -8,7 +10,7 @@ from qtpy.QtWidgets import QDialog, QHBoxLayout, QVBoxLayout, QWidget
 import qtextra.helpers as hp
 
 
-class QtPickOption(QDialog):
+class QtPickOptionBase(QDialog):
     """Select between options."""
 
     option: ty.Optional[str] = None
@@ -17,12 +19,20 @@ class QtPickOption(QDialog):
         super().__init__(parent)
         self.setWindowTitle("Select option")
 
+        self.text = text
         self.options = options
-
         self.responses = {}
-        btn_layout = QHBoxLayout()
+        self._setup_ui()
+
+    def _get_layout_widget(self) -> tuple[QWidget | None, QWidget | None]:
+        """Get layout widget."""
+        raise NotImplementedError("Must implement method")
+
+    def _setup_ui(self) -> None:
+        area, widget = self._get_layout_widget()
+        btn_layout = QHBoxLayout(area)
         btn_layout.addStretch(1)
-        for option, label in options.items():
+        for option, label in self.options.items():
             btn = hp.make_btn(
                 self, label, object_name="pick_option_button", func=partial(self.on_accept, option=option)
             )
@@ -31,9 +41,12 @@ class QtPickOption(QDialog):
         btn_layout.addStretch(1)
 
         layout = QVBoxLayout(self)
-        layout.addWidget(hp.make_label(self, text, enable_url=True))
+        layout.addWidget(hp.make_label(self, self.text, enable_url=True, wrap=True, object_name="pick_option_label"))
         layout.addStretch(1)
-        layout.addLayout(btn_layout)
+        if widget:
+            layout.addWidget(widget)
+        else:
+            layout.addLayout(btn_layout)
 
     def on_accept(self, option: str):
         """Set accepted."""
@@ -46,42 +59,23 @@ class QtPickOption(QDialog):
         super().reject()
 
 
-class QtScrollablePickOption(QDialog):
+class QtPickOption(QtPickOptionBase):
     """Select between options."""
 
-    option: ty.Optional[str] = None
+    def _get_layout_widget(self) -> tuple[QWidget | None, QWidget | None]:
+        """Get layout widget."""
+        return None, None
 
-    def __init__(self, parent: QWidget, text: str, options: ty.Dict[str, str]):
-        super().__init__(parent)
-        self.setWindowTitle("Select option")
+
+class QtScrollablePickOption(QtPickOptionBase):
+    """Select between options."""
+
+    def __init__(self, parent: QWidget, text: str, options: ty.Dict[ty.Any, str]):
+        super().__init__(parent, text, options)
         min_height = min(400, 40 * len(options) + 70)
-
         self.setMinimumSize(500, min_height)
 
-        self.options = options
-        self.responses = {}
-
+    def _get_layout_widget(self) -> tuple[QWidget | None, QWidget | None]:
+        """Get layout widget."""
         scroll_area, scroll_widget = hp.make_scroll_area(self)
-        scroll_layout = QVBoxLayout(scroll_area)
-        scroll_layout.addStretch(1)
-        for option, label in options.items():
-            btn = hp.make_btn(
-                self, label, object_name="pick_option_button", func=partial(self.on_accept, option=option)
-            )
-            scroll_layout.addWidget(btn)
-            self.responses[btn.text()] = option
-        scroll_layout.addStretch(1)
-
-        layout = QVBoxLayout(self)
-        layout.addWidget(hp.make_label(self, text, enable_url=True))
-        layout.addWidget(scroll_widget)
-
-    def on_accept(self, option: str):
-        """Set accepted."""
-        self.option = option
-        self.accept()
-
-    def reject(self):
-        """Set rejected."""
-        self.option = None
-        super().reject()
+        return scroll_area, scroll_widget
