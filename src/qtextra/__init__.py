5df1940626fabb1fa3dd665a60dc67f653de5363
Lukasz Migas <lukas.migas@yahoo.com>
2025-04-24 15:33:23 +0100

    cleanup

--- src/qtextra/__init__.py
+++ src/qtextra/__init__.py
@@ -2,9 +2,9 @@
 
 from loguru import logger
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 __author__ = "Lukasz G. Migas"
 __email__ = "lukas.migas@yahoo.com"
-__issue_url__ = "https://github.com/illumion-io/qtextra-issues/issues"
-__project_url__ = "https://ionglow.io/qtextra"
+__issue_url__ = "https://github.com/lukasz-migas/qtextra-issues/issues"
+__project_url__ = "https://lukasz-migas.com/qtextra"
 logger.disable("qtextra")
