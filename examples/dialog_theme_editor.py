ff30ccd2257ae93202302d71f54a263dda39f124
Lukasz Migas <lukas.migas@yahoo.com>
2026-03-31 17:10:48 +0100

    Update dialog_theme_editor.py

--- examples/dialog_theme_editor.py
+++ examples/dialog_theme_editor.py
@@ -1,19 +1,21 @@
 """DialogThemeEditor example."""
 
-from qtpy.QtWidgets import QApplication, QWidget
+from qtpy.QtWidgets import QApplication
 
 from qtextra.config import THEMES
 from qtextra.dialogs.qt_theme_editor import DialogThemeEditor
+from qtextra.dialogs.qt_theme_sample import QtSampleWidget
 
 app = QApplication([])
 
-preview_target = QWidget()
+preview_target = QtSampleWidget()
 preview_target.setWindowTitle("Theme target")
-preview_target.resize(900, 700)
 THEMES.apply(preview_target)
 preview_target.show()
 
 editor = DialogThemeEditor(None, dlg=preview_target)
-editor.show()
+editor.resize(300, 700)
+THEMES.apply(editor)
+editor.show_right_of_widget(preview_target, y_offset=editor.height())
 
 app.exec_()
