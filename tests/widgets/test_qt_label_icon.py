ad67f764e8e5076fff8c5a22040c1eca06c98f3c
Lukasz Migas <lukas.migas@yahoo.com>
2026-04-09 09:44:34 +0100

    Update test_qt_label_icon.py

--- tests/widgets/test_qt_label_icon.py
+++ tests/widgets/test_qt_label_icon.py
@@ -139,7 +139,7 @@ def test_qta_size_deprecations_warn_and_preserve_legacy_object_name(qapp, qtbot)
         widget.set_qta_size_preset("large")
     assert widget.objectName() == "large_icon"
 
-    with pytest.deprecated_call(match="get_icon_size_for_name"):
+    with pytest.deprecated_call(match="set_large"):
         object_name, size = QtQtaLabel.get_icon_size_for_name("large")
     assert object_name == "large_icon"
     assert size == (40, 40)
