43ae603eae43bdcb5bf7d24b1a3eee2713a33a38
Lukasz Migas <lukas.migas@yahoo.com>
2026-04-17 15:26:45 +0100

    Few fixes

--- tests/_test_helpers.py
+++
@@ -1,9 +0,0 @@
-from typing import Any, TypeVar
-
-T = TypeVar("T")
-
-
-def add_widget(qtbot: Any, widget: T) -> T:
-    """Register a widget with ``qtbot`` and return it."""
-    qtbot.addWidget(widget)
-    return widget
